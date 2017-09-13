80c0abe37775314793d921df8d6d18e960acd133
Ben Lovell <benjamin.lovell@gmail.com>
2017-09-13 11:15:42 +0100

    'Reserve' slots with associated appointments
    Ensure slots are not presented externally once associated with an
    appointment. They can be returned to the pool when the appointment is
    assigned to a different slot.

---
+++ spec/factories/appointments.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :appointment do
+    first_name 'Rick'
+    last_name 'Sanchez'
+    email 'rick@example.com'
+    phone '0208 252 4758'
+    opt_out_of_market_research true
+    dc_pot_confirmed true
+    memorable_word 'snootbooper'
+    date_of_birth '1945-01-01'
+    type_of_appointment '50-54'
+  end
+end
