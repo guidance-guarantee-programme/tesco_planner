a185893275f5b33b186228dce1c20c8abf9a44f2
Ben Lovell <benjamin.lovell@gmail.com>
2017-09-26 13:06:13 +0100

    Restrict external locations to `active` only
    Ensures administrators can work on locations in the background until
    they're ready to launch.

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class LocationsController < ActionController::Base
       def index
-        render json: Location.order(:name).all
+        render json: Location.active.order(:name).all
       end
 
       def show
@@ -12,7 +12,7 @@ module Api
       private
 
       def location
-        @location ||= Location.find(params[:id])
+        @location ||= Location.active.find(params[:id])
       end
     end
   end
