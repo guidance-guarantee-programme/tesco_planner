100b6a29785ed2c2dbeb3756616f34ef01f51a1c
Ben Lovell <benjamin.lovell@gmail.com>
2017-10-13 10:36:56 +0100

    Add identifying mailgun variables to emails
    This is to identify drops when inbound webhooks are triggered.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,14 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'Tesco Pension Wise Appointments <appointments@pensionwise.gov.uk>'
 
   layout 'mailer'
+
+  protected
+
+  def mailgun_headers(message_type, appointment)
+    headers['X-Mailgun-Variables'] = {
+      message_type: message_type,
+      appointment_id: appointment.to_param,
+      environment: Rails.env
+    }.to_json
+  end
 end
