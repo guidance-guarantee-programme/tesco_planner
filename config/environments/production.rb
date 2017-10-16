896a76760753eaac7af0a420abd4a587b9b51a91
Ben Lovell <benjamin.lovell@gmail.com>
2017-10-17 00:27:38 +0100

    Configure default URL options
    For mailers.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
 
   # Configure email delivery method
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: ENV.fetch('APPLICATION_HOST') }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
