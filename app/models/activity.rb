d6af080cb47b09be48a86e8dd94021ecdec73902
Ben Lovell <benjamin.lovell@gmail.com>
2018-03-21 10:42:22 +0000

    Remove unnecessary default scope ordering

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,8 +2,6 @@ class Activity < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :appointment
 
-  default_scope { order(created_at: :desc) }
-
   def to_partial_path
     "activities/#{model_name.singular}"
   end
