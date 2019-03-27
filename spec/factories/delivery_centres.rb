4c4c1a5adf97c1b7422341255708aed17733f4cf
Ben Lovell <benjamin.lovell@gmail.com>
2019-03-27 11:59:26 +0000

    Address `factory_bot_rails` deprecations

--- spec/factories/delivery_centres.rb
+++ spec/factories/delivery_centres.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :delivery_centre do
     sequence(:name) { |n| "Delivery Centre #{n}" }
-    reply_to 'dc@example.com'
-    hidden false
+    reply_to { 'dc@example.com' }
+    hidden { false }
 
     trait :hidden do
-      hidden true
+      hidden { true }
     end
 
     trait :with_locations do
--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :location do
     sequence(:name) { |n| "Tesco #{n}" }
 
-    address_line_one '1 Some Street'
-    address_line_two 'Some Place'
-    address_line_three ''
-    town 'Some Town'
-    county 'Some County'
-    postcode 'RG2 9FL'
+    address_line_one { '1 Some Street' }
+    address_line_two { 'Some Place' }
+    address_line_three { '' }
+    town { 'Some Town' }
+    county { 'Some County' }
+    postcode { 'RG2 9FL' }
     delivery_centre
 
     after(:build) { |location| location.rooms << build(:room) }
