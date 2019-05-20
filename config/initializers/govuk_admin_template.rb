892da769e3dfb4c7f6379a10014bb9220bf90364
Ben Lovell <benjamin.lovell@gmail.com>
2019-05-20 13:04:51 +0100

    Remove Tesco reference from app title

--- config/initializers/govuk_admin_template.rb
+++ config/initializers/govuk_admin_template.rb
@@ -1,7 +1,7 @@
 GovukAdminTemplate.environment_style = Rails.env.staging? ? 'preview' : ENV['RAILS_ENV']
 
 GovukAdminTemplate.configure do |c|
-  c.app_title = 'Tesco Appointment Planner'
+  c.app_title = 'Employer Planner'
   c.show_flash = true
   c.show_signout = true
 end
