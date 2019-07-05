e2329cc75b5f4e35527e738ebb7863319baea1fe
Ben Lovell <benjamin.lovell@gmail.com>
2019-07-05 13:39:00 +0100

    Ensure the correct locations are included
    We need to filter the locations based on the current user's granted
    access. Previously this would display all locations assigned to the
    employer regardless whether the current user was permitted to see them.

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -2,4 +2,14 @@ module LocationsHelper
   def location_options(scope)
     scope.order(:name).pluck(:name)
   end
+
+  def grouped_employer_locations(current_user)
+    [].tap do |data|
+      current_user.delivery_centre.employers.order(:name).each do |employer|
+        filtered_locations = employer.locations.where(id: current_user.locations.pluck(:id)).order(:name)
+
+        data << [employer.name, filtered_locations.pluck(:name, :id)]
+      end
+    end
+  end
 end
