21acd35ffcc0f7774e38aa333000db515b55dd0f
Ben Lovell <benjamin.lovell@gmail.com>
2020-08-21 15:10:22 +0100

    Add a rescheduled notice to emails
    Confirmation emails know if the appointment has been rescheduled and
    will customise the content if necessary.

--- spec/mailers/appointment_mailer_spec.rb
+++ spec/mailers/appointment_mailer_spec.rb
@@ -58,9 +58,22 @@ RSpec.describe AppointmentMailer do
   end
 
   describe '#customer' do
+    let(:rescheduled) { false }
+
     subject do
       travel_to '2017-09-16 13:00 UTC' do
-        described_class.customer(appointment)
+        described_class.customer(appointment, rescheduled: rescheduled)
+      end
+    end
+
+    context 'when it has been rescheduled' do
+      let(:rescheduled) { true }
+
+      it 'contains the necessary detail' do
+        subject.body.encoded.tap do |body|
+          expect(body).to include('We’ve rescheduled your appointment')
+          expect(body).to include('We’ve updated your appointment')
+        end
       end
     end
 
@@ -72,6 +85,8 @@ RSpec.describe AppointmentMailer do
       expect(subject.to).to match_array('rick@example.com')
 
       subject.body.encoded.tap do |body|
+        expect(body).to include('We’ve booked your appointment')
+        expect(body).to include('Thank you for booking')
         expect(body).to include('Dear Rick')
         expect(body).to include('16 September 2017')
         expect(body).to include('2:00pm')
