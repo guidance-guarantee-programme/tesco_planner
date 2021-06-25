88165fc614439f9b5b23f8a0e75ef5034fb57e77
Ben Lovell <benjamin.lovell@gmail.com>
2021-06-25 14:13:55 +0100

    Theme confirmation email for MoneyHelper

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = "https://#{ENV.fetch('APPLICATION_HOST')}"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
