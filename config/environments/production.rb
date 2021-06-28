af3cde758e00b664816914482b0ef7e58d67d8f0
Ben Lovell <benjamin.lovell@gmail.com>
2021-06-28 10:36:33 +0100

    Configure mailgun domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
     address: ENV['MAILGUN_SMTP_SERVER'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: 'pensionwise.gov.uk',
+    domain: 'moneyhelper.org.uk',
     authentication: :plain
   }
 
